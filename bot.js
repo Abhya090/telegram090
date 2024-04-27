ed3651d6ec6502688256aad3db66f06fff12159f
Abhay ubale <120577368+Abhya090@users.noreply.github.com>
2024-04-27 18:48:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
 const TelegramBot = require("node-telegram-bot-api");
 const fs = require("fs");
+const dotenv = require("dotenv");
 const { Client } = require("ssh2");
+dotenv.config();
 
 const TOKEN = process.env.BOT_TOKEN || "";
 const CHAT_ID = process.env.CHAT_ID || "";
+const OWNER_IDS = process.env.OWNER_IDS || "";
 
 const SERVERS_FILE = "servers.json";
 
@@ -16,10 +19,10 @@ const ssh = new Client();
 let pwd = "~";
 
 ssh.on("ready", async () => {
-  await bot.sendMessage(CHAT_ID, "SSH connection established.");
+  await bot.sendMessage(CHAT_ID, "ssh successfully.");
 });
 
-const sshExecute = (command, ping) => {
+const sshExcute = (command, ping) => {
   const conmm = `cd ${pwd} && ${command}`;
 
   ssh.exec(conmm, (err, stream) => {
@@ -71,21 +74,29 @@ if (fs.existsSync(SERVERS_FILE)) {
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 async function checkOwner(msg) {
-  if (msg?.chat?.id.toString() !== CHAT_ID) {
+  if (!OWNER_IDS.includes(msg?.chat?.id)) {
     await bot.sendMessage(CHAT_ID, `Got other access\n${JSON.stringify(msg)}`);
     return false;
   }
   return true;
 }
 
-bot.setMyCommands([
-  { command: "add", description: "add new server /add user@abc.com" },
-  { command: "list", description: "list servers" },
-  { command: "current", description: "current server" },
-  { command: "rm", description: "remove server" },
-  { command: "connect", description: "/connect username@hostip" },
-  { command: "exit", description: "exit" },
-]);
+// bot.getMe().then((res) => {
+//   console.log(res);
+// });
+
+bot
+  .setMyCommands([
+    { command: "add", description: "add new an server /add user@abc.com" },
+    { command: "list", description: "list server" },
+    { command: "current", description: "current server" },
+    { command: "rm", description: "remove server" },
+    { command: "connect", description: "/connect ID | IP" },
+    { command: "exit", description: "exit" },
+  ])
+  .then((res) => {
+    console.log(res);
+  });
 
 // CRUD
 bot.onText(/\/list/, async (msg) => {
@@ -93,7 +104,7 @@ bot.onText(/\/list/, async (msg) => {
   if (!o) {
     return;
   }
-  let message = `List of Servers: ${servers.length}\n`;
+  let message = `List Server: ${servers.length}\n`;
   servers.forEach((s, i) => {
     message += `${i + 1}: ${s}\n`;
   });
@@ -111,55 +122,101 @@ bot.onText(/\/current/, async (msg) => {
   if (!current) {
     await bot.sendMessage(
       CHAT_ID,
-      `No server is currently selected. Please connect to a server first.`
+      `No server now, please connect one server before next.`
     );
     return;
   }
-  await bot.sendMessage(CHAT_ID, `Current server: ${current}`, {
+  await bot.sendMessage(CHAT_ID, `Current: ${current}`, {
     disable_web_page_preview: true,
     protect_content: true,
   });
 });
+bot.onText(/\/add (.+)/, async (msg, match) => {
+  const o = await checkOwner(msg);
+  if (!o) {
+    await bot.sendMessage(CHAT_ID, "Value is invalid.");
+    return;
+  }
+  const sv = match[1].trim().toLocaleLowerCase();
+  if (sv.includes("@")) {
+    servers.push(sv);
+    fs.writeFileSync(SERVERS_FILE, JSON.stringify(servers), "utf8");
+    await bot.sendMessage(CHAT_ID, `Add ${sv} success`, {
+      disable_web_page_preview: true,
+      protect_content: true,
+    });
+  } else {
+    await bot.sendMessage(CHAT_ID, `${sv} is not valid`, {
+      disable_web_page_preview: true,
+      protect_content: true,
+    });
+  }
+});
+bot.onText(/\/rm (.+)/, async (msg, match) => {
+  const o = await checkOwner(msg);
+  if (!o) {
+    return;
+  }
+  const sv = match[1].trim().toLocaleLowerCase();
+  let find = null;
+  if (
+    sv.includes("@") &&
+    servers.length !== servers.filter((s) => s !== sv).length
+  ) {
+    find = sv;
+  } else {
+    const index = parseFloat(sv) - 1;
+    find = servers[index];
+  }
+
+  if (find) {
+    servers = servers.filter((s) => s !== find);
+    fs.writeFileSync(SERVERS_FILE, JSON.stringify(servers), "utf8");
+    await bot.sendMessage(CHAT_ID, `Remove ${sv} success`, {
+      disable_web_page_preview: true,
+      protect_content: true,
+    });
+  } else {
+    await bot.sendMessage(CHAT_ID, `${sv} is not valid`, {
+      disable_web_page_preview: true,
+      protect_content: true,
+    });
+  }
+});
+
+// ssh
 bot.onText(/\/connect (.+)/, async (msg, match) => {
   const o = await checkOwner(msg);
   if (!o) {
     return;
   }
-  const connectionInfo = match[1].trim();
-  const [username, host] = connectionInfo.split('@');
-  
-  if (!username || !host) {
-    await bot.sendMessage(CHAT_ID, "Invalid format. Please use /connect username@hostip");
-    return;
+  const sv = match[1].trim().toLocaleLowerCase();
+  let find = null;
+  if (
+    sv.includes("@") &&
+    servers.length !== servers.filter((s) => s !== sv).length
+  ) {
+    find = sv;
+  } else {
+    const index = parseFloat(sv) - 1;
+    find = servers[index];
   }
-
-  current = `${username}@${host}`;
-  // Prompt the user to send the private key file for authentication
-  await bot.sendMessage(CHAT_ID, `Please send your private key file for ${current}`);
-});
-
-// Listen for private key file messages
-bot.on("document", async (msg) => {
-  if (current && msg.document) {
-    const fileId = msg.document.file_id;
-    const file = await bot.getFile(fileId);
-    const filePath = file.file_path;
-    const privateKeyPath = `./${msg.from.id}_${msg.document.file_name}`;
-    await bot.downloadFile(filePath, privateKeyPath);
-
-    // Connect to the server using the provided details and private key file
-    const [username, host] = current.split('@');
+  if (find) {
+    current = find;
+    // try connect to current server
+    const info = current.split("@");
     ssh.connect({
-      host: host,
-      username: username,
-      privateKey: fs.readFileSync(privateKeyPath),
+      host: info[1],
+      username: info[0],
+      privateKey: fs.readFileSync(process.env.PATH_PRIVATEKEY),
     });
-
-    // Delete the private key file after use
-    fs.unlinkSync(privateKeyPath);
+  } else {
+    await bot.sendMessage(CHAT_ID, `${sv} is not valid`, {
+      disable_web_page_preview: true,
+      protect_content: true,
+    });
   }
 });
-
 bot.onText(/\/exit/, async (msg, match) => {
   const o = await checkOwner(msg);
   if (!o) {
@@ -182,13 +239,13 @@ bot.on("text", async (msg) => {
   if (!current && ssh) {
     await bot.sendMessage(
       CHAT_ID,
-      `No server is currently selected. Please connect to a server first.`
+      `No server now, please connect one server before next.`
     );
     return;
   }
-  const ping = await bot.sendMessage(CHAT_ID, `Executing command...`);
+  const ping = await bot.sendMessage(CHAT_ID, `exec...`);
   try {
-    sshExecute(msg.text.trim(), ping);
+    sshExcute(msg.text.trim(), ping);
   } catch (error) {
     console.log(error);
     await bot.editMessageText(`Error: ${JSON.stringify(error, null, 2)}`, {
@@ -198,7 +255,7 @@ bot.on("text", async (msg) => {
   }
 });
 
-// Helper
+// helper
 function isBotCommand(message) {
   if (!message || !message.entities) {
     return false;
@@ -207,4 +264,4 @@ function isBotCommand(message) {
     (entity) => entity.type === "bot_command"
   );
   return botCommands.length > 0;
-    }
+}
